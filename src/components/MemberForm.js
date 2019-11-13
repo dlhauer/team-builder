f249783fcf770caf2a7805a03536a8c446952360
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-11-13 15:53:28 -0800

    Added form validation

--- src/components/MemberForm.js
+++ src/components/MemberForm.js
@@ -10,6 +10,7 @@ function MemberForm(props) {
   } );
 
   function handleChange(e) {
+
     setMember({...member, [e.target.name]: e.target.value});
   }
 
@@ -24,30 +25,43 @@ function MemberForm(props) {
     } )
   }
 
+  //Why oh why?!
+
+  // let nameInput = document.getElementById('name');
+  // console.log(nameInput);
+  // nameInput.oninvalid = function(e) {
+  //   e.target.setCustomValidity('Name must contain between 2 and 30 letters, no numbers or special characters.');
+  // }
+
   return (
     <form onSubmit={submitForm}>
       <label htmlFor='name'>Name </label>
       <input 
+        required
         id='name'
         type='text'
         name='name'
+        pattern='[A-Za-z\s]{2,30}'
+        title='Name must contain at between 2 and 30 letters, no numbers or special characters.'
         placeholder='member name goes here, doofus'
         onChange={handleChange}
         value={member.name}
       />
       <label htmlFor='role'>Role </label>
       <select
+        required
         id='role'
         name='role'
         onChange={handleChange}
         value={member.role}>
         <option value=''></option>
-        <option value='Hellraiser'>Hell Raiser</option>
+        <option value='Hellraiser'>Hellraiser</option>
         <option value='Rabble Rouser'>Rabble Rouser</option>
         <option value='Hooligan'>Hooligan</option>
       </select>
       <label htmlFor='email'>Email </label>
       <input 
+        required
         id='email'
         type='email'
         name='email'
@@ -57,9 +71,12 @@ function MemberForm(props) {
       />
       <label htmlFor='catchphrase'>Catchphrase </label>
       <input 
+        required
         id='catchphrase'
         type='textarea'
         name='catchphrase'
+        pattern='[A-Za-z]+[A-Za-z.?,\s]*{3,120}' //How to do an apostrophe?
+        title='Catchphrase must contain between 3 and 120 letters, no bizarre punctuation, and no numbers.'
         onChange={handleChange}
         value={member.catchphrase}
       />
